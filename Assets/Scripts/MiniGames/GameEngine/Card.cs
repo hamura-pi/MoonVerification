ee48a1e87dbbec73d12ebc3c0e22e7d602c429c4
halmikar <shpunsu@gmail.com>
2020-02-03 21:27:50 +0300

    Init

---
+++ Assets/Scripts/MiniGames/GameEngine/Card.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.PlayerLoop;
+
+namespace MiniGames.GameEngine
+{   
+    [RequireComponent(typeof(Animator))]
+    public class Card : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler
+    {
+        public const float CardSize = 0.4731f;
+        
+        public CardData Data;
+        public SpriteRenderer FaceSriteRenderer;
+        public Transform target;
+
+        public bool IsOpenCard => _isOpenCard;
+       
+
+        private CardDeck _cardDeck;
+        private bool _isOpenCard;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            FaceSriteRenderer.sprite = Data.FaceCard;
+            FaceSriteRenderer.size = new Vector2(CardSize, CardSize);
+            
+            _isOpenCard = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*if (Input.GetKeyDown(KeyCode.O))
+            {
+                transform.DOMove(target.position, 5, false);
+            }*/
+        }
+        
+        #region ImplementInput
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("<color=blue>" + gameObject.name + "</color>");
+            var s = DOTween.Sequence();
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            
+        }
+
+        #endregion
+    }
+
+    [Serializable]
+    public struct CardData
+    {
+        [SerializeField]
+        private Sprite faceCard;
+        public int CardId;
+
+        public Sprite FaceCard
+        {
+            get { return faceCard; }
+            set
+            {
+                faceCard = value;
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/MiniGames/Memory/CardsCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MiniGames.GameEngine;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CollectionCard", menuName = "Create Collection Card", order = 12)]
+public class CardsCollection : ScriptableObject
+{
+    public CardData[] data;
+}
